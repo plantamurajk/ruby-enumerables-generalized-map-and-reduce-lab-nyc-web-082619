cd736e5b363b801d50f895eb5ccf9e2c15554134
John Plantamura <plantamurajk@gmail.com>
2019-08-14 23:41:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,6 @@
 # Your Code Here
 
 def my_own_map(source_array)
-  negative_array = source_array.map{|num| num * -1}
+  negative_array = source_array.map {|num| num * -1}
   return negative_array
 end
