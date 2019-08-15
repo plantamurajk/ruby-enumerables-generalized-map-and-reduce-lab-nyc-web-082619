7d4d1e332fc38ccb5ad60ff75c5cc91f1b99b6d9
John Plantamura <plantamurajk@gmail.com>
2019-08-15 00:50:36 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
 # Your Code Here
+
+def map(source_array, &block)
+  new_array = source_array.map(&block)
+  return new_array
+end
+
+def reduce(source_array, starting_point = 0, &block)
+  total = source_array.reduce(&block)
+  if (total.class == Integer) then
+    return total + starting_point
+  end
+  return total
+end
