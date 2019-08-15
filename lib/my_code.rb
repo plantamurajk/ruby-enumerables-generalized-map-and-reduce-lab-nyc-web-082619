fa65734fcd8671ff3d0a66eb41f181ae44d71659
John Plantamura <plantamurajk@gmail.com>
2019-08-15 00:36:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,6 +6,6 @@ def map(source_array, &block)
 end
 
 def reduce(source_array, starting_point = 0, &block)
-  total += source_array.reduce(&block)
+  total = source_array.reduce(&block)
   return total
 end
